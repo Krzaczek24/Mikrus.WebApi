9359520633942e154a46113dcb5897d33f903480
Tomasz 'Krzaczek' Drewek <tomaszdrewek94@gmail.com>
2025-12-13 20:43:40 +0100

    Redirection to swagger json file

---
+++ Krzaq.Mikrus.WebAPI/Controllers/IndexController.cs
@@ -0,0 +1,14 @@
+﻿using Krzaq.Mikrus.WebAPI;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Krzaq.Mikrus.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class IndexController : ControllerBase
+    {
+        [HttpGet("/")]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public RedirectResult RedirectToJson() => Redirect(Program.ENDPOINT);
+    }
+}
--- Krzaq.Mikrus.WebAPI/Program.cs
+++ Krzaq.Mikrus.WebAPI/Program.cs
@@ -2,19 +2,23 @@ namespace Krzaq.Mikrus.WebAPI
 {
     public class Program
     {
-        private const string documentName = "swagger";
+        private const string DOC_NAME = "swagger";
+        public const string ENDPOINT = $"/openapi/{DOC_NAME}.json";
 
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             builder.Services.AddControllers();
-            builder.Services.AddOpenApi(documentName);
+            builder.Services.AddOpenApi(DOC_NAME);
 
             var app = builder.Build();
 
-            app.MapOpenApi();
-            app.UseSwaggerUI(opts => opts.SwaggerEndpoint($"/openapi/{documentName}.json", nameof(Krzaq.Mikrus.WebAPI)));
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+                app.UseSwaggerUI(opts => opts.SwaggerEndpoint(ENDPOINT, nameof(WebAPI)));
+            }
 
             app.UseAuthorization();
 
